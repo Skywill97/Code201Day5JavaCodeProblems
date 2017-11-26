36b518443a7512745a6786cb5afdab9d330f09bf
Skyler Williams <kaiser1997@gmail.com>
2017-11-26 01:30:43 -0800

    q2and3done

--- app.js
+++ app.js
@@ -6,16 +6,14 @@ Write a function called sum() that takes in two numbers as arguments and then re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSum() function below and check the console to see if the test passes.*/
 
 // Write your code here
-var a = 4;
-var b = 7;
-function sum(a,b) {
-  console.log('a','b');
-  return a + b;
-}
-sum(a, b);
+function testSum(a, b) { //eslint-disable-line
+  var sumPlus = a + b;
+  var array = ['The sum of '+ a +' plus ' + b + ' is ' + sumPlus + '.'];
+  return array;
 
+}
 // Here is the test for sum(); uncomment it to run it
-// testSum(4, 7);
+testSum(4, 7);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -26,15 +24,14 @@ Write a function called multiply() that takes in two numbers as arguments and re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiply() function and see if the test passes.*/
 
 // Write your code here
-var c = 5;
-var d = 9;
-function multiply(c,d) {
-  console.log('c','d');
-  return c + d;
+function testMultiply(a, b) { //eslint-disable-line
+  var multi = a * b;
+  var array = ['The product of ' + a + ' multiplied by ' + b + ' is ' + multi + '.'];
+  return array;
+
 }
-multiply(c , d);
 //Here is the test for multiply(); uncomment it to run it
-//testMultiply(5,9);
+testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
